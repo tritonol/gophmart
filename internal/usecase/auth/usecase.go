45324abe76a110ad592197552904be635ff347ca
Кирилл Сабуров <kirillsaburov@MacBook-Pro-Kirill-2.local>
2024-09-09 12:49:19 +0300

    feat: implements auth usecase

---
+++ internal/usecase/auth/usecase.go
@@ -0,0 +1,118 @@
+package auth
+
+import (
+	"context"
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/tritonol/gophmart.git/internal/models"
+)
+
+const (
+	salt      = "abcd"
+	secretJwt = "secret"
+)
+
+type Claims struct {
+	jwt.RegisteredClaims
+	Id models.UserID
+}
+
+type authUsecase struct {
+	repo AuthRepo
+}
+
+type AuthRepo interface {
+	Create(ctx context.Context, credentials models.UserCredentials) (models.UserID, error)
+	CheckByCredentials(ctx context.Context, credentials models.UserCredentials) (models.UserID, error)
+}
+
+func New(repo AuthRepo) *authUsecase {
+	return &authUsecase{
+		repo: repo,
+	}
+}
+
+func (uc *authUsecase) Register(ctx context.Context, credetials models.UserCredentials) (string, error) {
+	if credetials.Login == "" && credetials.Password == "" {
+		return "", fmt.Errorf("wrong credentials")
+	}
+
+	hashedPass, err := sha1Hash(credetials.Password)
+	if err != nil {
+		return "", err
+	}
+	credetials.Password = hashedPass
+
+	userId, err := uc.repo.Create(ctx, credetials)
+	if err != nil {
+		return "", err
+	}
+
+	return buildJwt(userId)
+}
+
+func (uc *authUsecase) Login(ctx context.Context, credentials models.UserCredentials) (string, error) {
+	hashedPass, err := sha1Hash(credentials.Password)
+	if err != nil {
+		return "", err
+	}
+	credentials.Password = hashedPass
+
+	userId, err := uc.repo.CheckByCredentials(ctx, credentials)
+	if err != nil {
+		return "", nil
+	}
+
+	return buildJwt(userId)
+}
+
+func (uc *authUsecase) ValidateToken(token string) (models.UserID, error) {
+	claims := &Claims{}
+	parsedToken, err := jwt.ParseWithClaims(token, claims,
+		func(t *jwt.Token) (interface{}, error) {
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+			}
+			return []byte(secretJwt), nil
+		})
+
+	if err != nil {
+		return 0, err
+	}
+	if !parsedToken.Valid {
+		return 0, fmt.Errorf("token is invalid")
+	}
+
+	return claims.Id, nil
+}
+
+func sha1Hash(pass string) (string, error) {
+	hash := sha1.New()
+	if _, err := hash.Write([]byte(pass)); err != nil {
+		return "", err
+	}
+
+	hashedBytes := hash.Sum([]byte(salt))
+	hashedString := hex.EncodeToString(hashedBytes)
+
+	return hashedString, nil
+}
+
+func buildJwt(id models.UserID) (string, error) {
+	token := jwt.NewWithClaims(
+		jwt.SigningMethodHS256,
+		Claims{
+			Id: id,
+		},
+	)
+
+	tokenStr, err := token.SignedString([]byte(secretJwt))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenStr, nil
+}
