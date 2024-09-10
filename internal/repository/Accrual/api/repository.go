b0bbf4f2b80aec4f63cbe0d021c2a2117e0e0a8c
Кирилл Сабуров <kirillsaburov@MacBook-Pro-Kirill-2.local>
2024-09-10 16:16:57 +0300

    feat: implements repo-api for accrual

---
+++ internal/repository/Accrual/api/repository.go
@@ -0,0 +1,45 @@
+package api
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/tritonol/gophmart.git/internal/models"
+)
+
+type Accrual struct {
+	client resty.Client
+}
+
+func New(address string) *Accrual {
+	return &Accrual{
+		client: *resty.New().SetBaseURL(address),
+	}
+}
+
+func (a *Accrual) GetAccrual(ctx context.Context, orderId int64) (*models.Accrual, error) {
+	res := &models.Accrual{}
+
+	resp, err := a.client.R().
+		SetContext(ctx).
+		SetResult(res).
+		Get("api/orders/" + strconv.FormatInt(orderId, 10))
+
+	if err != nil {
+		return nil, err
+	}
+
+	switch resp.StatusCode() {
+	case http.StatusNoContent:
+		return nil, fmt.Errorf("order not found")
+	case http.StatusTooManyRequests:
+		return nil, fmt.Errorf("too many requests")
+	case http.StatusOK:
+		return res, nil
+	default:
+		return nil, fmt.Errorf("status code not definde")
+	}
+}
