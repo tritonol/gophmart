e8920f5b6c3fa1bf351a37e97c23b4998976dc8e
Кирилл Сабуров <kirillsaburov@MacBook-Pro-Kirill-2.local>
2024-09-10 16:37:31 +0300

    feat: add balance migration

---
+++ internal/storage/migrator/migrations/20240910133216_create_balance_table.sql
@@ -0,0 +1,20 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS "balance" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INTEGER,
+    "from" VARCHAR,
+    "from_id" INTEGER,
+    "value" INTEGER,
+    "type" INTEGER,
+
+    CONSTRAINT fk_user_id
+    FOREIGN KEY (user_id)
+    REFERENCES users (id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP IF EXISTS balance;
+-- +goose StatementEnd
