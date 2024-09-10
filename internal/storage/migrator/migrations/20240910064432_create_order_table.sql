2586d4f3c788cee45304e6f2349814f363372013
Кирилл Сабуров <kirillsaburov@MacBook-Pro-Kirill-2.local>
2024-09-10 10:17:59 +0300

    feat: create migration

---
+++ internal/storage/migrator/migrations/20240910064432_create_order_table.sql
@@ -0,0 +1,19 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS "orders" (
+    "id" BIGINT PRIMARY KEY,
+    "user_id" VARCHAR,
+    "status" VARCHAR,
+    "created_at" TIMESTAMP without time zone default (now() at time zone 'utc'),
+
+    CONSTRAINT fk_user_id
+    FOREIGN KEY (user_id)
+    REFERENCES user (id)
+);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_user_login ON users(login);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP IF EXISTS orders;
+-- +goose StatementEnd
