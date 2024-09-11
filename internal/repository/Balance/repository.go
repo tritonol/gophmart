2afda3f109d2378848bedc53a7779c797e994f8f
Кирилл Сабуров <kirillsaburov@MacBook-Pro-Kirill-2.local>
2024-09-11 14:28:38 +0300

    feat: added method for getting value from accrual

--- internal/repository/Balance/repository.go
+++ internal/repository/Balance/repository.go
@@ -69,3 +69,19 @@ func (r *BalanceRepo) GetTotalSpent(ctx context.Context, userId int64) (float64,
 
 	return sum, nil
 }
+
+func (r *BalanceRepo) GetByOrder(ctx context.Context, orderId int64) (float64, error) {
+	var val float64
+
+	err := r.conn.QueryRowContext(
+		ctx,
+		`SELECT value FROM balance WHERE from_id = $1`,
+		orderId,
+	).Scan(&val)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return val, nil
+}
