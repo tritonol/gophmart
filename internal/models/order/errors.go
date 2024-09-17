6c3665be0e7c47c000b9695287db09da63c9a6e2
Кирилл Сабуров <kirillsaburov@MacBook-Pro-Kirill-2.local>
2024-09-17 11:33:11 +0300

    fix

--- internal/models/order/errors.go
+++ internal/models/order/errors.go
@@ -2,6 +2,6 @@ package order
 
 import "errors"
 
-var ErrAlreadyExists = errors.New("order repository: order already exists for user")
+var ErrAlreadyExists = errors.New("order already exists for user")
 
-var ErrCreatedByAnotherUser = errors.New("order repository: order created by another user")
+var ErrCreatedByAnotherUser = errors.New("order created by another user")
