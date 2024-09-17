aabf231cfe84bda30cf134b651bb1bde93afc4de
Кирилл Сабуров <kirillsaburov@MacBook-Pro-Kirill-2.local>
2024-09-17 16:44:08 +0300

    refactor: remove unused method

--- internal/repository/Balance/repository.go
+++ internal/repository/Balance/repository.go
@@ -64,34 +64,18 @@ func (r *BalanceRepo) GetTotalSpent(ctx context.Context, userId int64) (float64,
 	return sum, nil
 }
 
-func (r *BalanceRepo) GetWithdrawals(ctx context.Context, orderId int64, userId int64) ([]*balance.Transaction ,error) {
+func (r *BalanceRepo) GetWithdrawals(ctx context.Context, userId int64) ([]*balance.Transaction ,error) {
 	withdrawals := make([]*balance.Transaction, 0)
 	query := `
 		SELECT id, user_id, from_id, value, processed_at 
-		FROM balance WHERE user_id = $1 AND from_id = $2
+		FROM balance WHERE user_id = $1
 		WHERE value < 0
 	`
 
-	err := r.conn.SelectContext(ctx, &withdrawals, query, orderId, userId)
+	err := r.conn.SelectContext(ctx, &withdrawals, query, userId)
 	if err != nil {
 		return nil, err
 	}
 
 	return withdrawals, nil
-}
-
-func (r *BalanceRepo) GetByOrder(ctx context.Context, orderId int64) (float64, error) {
-	var val float64
-
-	err := r.conn.QueryRowContext(
-		ctx,
-		`SELECT value FROM balance WHERE from_id = $1`,
-		orderId,
-	).Scan(&val)
-
-	if err != nil {
-		return 0, err
-	}
-
-	return val, nil
 }
