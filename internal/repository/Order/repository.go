d0cb1f2f6c66727ccab48092b4b299f254d4cfc7
Кирилл Сабуров <kirillsaburov@MacBook-Pro-Kirill-2.local>
2024-09-18 15:56:06 +0300

    fix

--- internal/repository/Order/repository.go
+++ internal/repository/Order/repository.go
@@ -3,7 +3,6 @@ package order
 import (
 	"context"
 	"errors"
-	"fmt"
 	"time"
 
 	"github.com/jackc/pgerrcode"
@@ -34,15 +33,13 @@ func New(ctx context.Context, db *sqlx.DB) *OrderRepo {
 func (r *OrderRepo) Create(ctx context.Context, model *models.Order) error {
 	order := toOrder(model, time.Now())
 
-	var userId int64
-
 	_, err := r.conn.ExecContext(ctx, `
 		INSERT INTO orders(id, user_id, status, uploaded_at)
 		VALUES ($1, $2, $3, $4) RETURNING user_id
 		`,
 		order.Id, order.UserId, order.Status, order.UploadedAt,
 	)
-	fmt.Println(userId, model.UserId)
+
 	if err != nil {
 		var pgErr *pgconn.PgError
 		if errors.As(err, &pgErr) && pgerrcode.UniqueViolation == pgErr.Code {
@@ -50,7 +47,6 @@ func (r *OrderRepo) Create(ctx context.Context, model *models.Order) error {
 			if err != nil {
 				return err
 			}
-			fmt.Println(existsForUser)
 			if existsForUser {
 				return models.ErrAlreadyExists
 			} else {
@@ -66,12 +62,13 @@ func (r *OrderRepo) Create(ctx context.Context, model *models.Order) error {
 func (r *OrderRepo) GetUserOrders(ctx context.Context, userId user.UserID) ([]*models.Order, error) {
 	res := make([]order, 0)
 	query := `
-		SELECT o.*, b.value FROM orders o
-		JOIN balance b ON o.id = b.from_id
+		SELECT o.*, COALESCE(b.value, 0) AS value FROM orders o
+		LEFT JOIN balance b ON o.id = b.from_id
 		WHERE o.user_id = $1
 		ORDER BY uploaded_at
 	`
 	err := r.conn.SelectContext(ctx, &res, query, userId)
+
 	if err != nil {
 		return nil, err
 	}
