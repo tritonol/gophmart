5ef930edd8aec3252e4e41c88b4380d23c12d7f8
Кирилл Сабуров <kirillsaburov@MacBook-Pro-Kirill-2.local>
2024-09-18 14:22:59 +0300

    app

--- internal/app/app.go
+++ internal/app/app.go
@@ -5,32 +5,47 @@ import (
 	"fmt"
 
 	"github.com/tritonol/gophmart.git/internal/config"
+	httpserver "github.com/tritonol/gophmart.git/internal/http"
+	apiAccrual "github.com/tritonol/gophmart.git/internal/repository/Accrual/api"
+	repoBalance "github.com/tritonol/gophmart.git/internal/repository/Balance"
+	repoOrder "github.com/tritonol/gophmart.git/internal/repository/Order"
 	repoUser "github.com/tritonol/gophmart.git/internal/repository/User"
 	"github.com/tritonol/gophmart.git/internal/storage/pg"
+	"github.com/tritonol/gophmart.git/internal/usecase/accrual"
 	"github.com/tritonol/gophmart.git/internal/usecase/auth"
+	"github.com/tritonol/gophmart.git/internal/usecase/balance"
+	"github.com/tritonol/gophmart.git/internal/usecase/orders"
 )
 
 func Run() {
 	ctx := context.Background()
-	// TODO: init logger
 
-	// TODO: add logging
 	cfg, err := config.MustLoad()
 	if err != nil {
 		panic("can't init config")
 	}
 
-	// TODO: add logging
 	db, err := pg.NewConnection(ctx, cfg.DbUri)
 	if err != nil {
 		fmt.Println(err)
 		panic("can't init database")
 	}
 
-	// TODO: init repos
 	repoAuth := repoUser.New(ctx, db)
+	repoOrder := repoOrder.New(ctx, db)
+	repoBalance := repoBalance.New(ctx, db)
 
-	// TODO: init usecases
+	repoAccrual := apiAccrual.New(cfg.AccrualAddress)
+
 	authUc := auth.New(repoAuth)
-	_ = authUc
+	ordersUc := orders.New(repoOrder)
+	accruarUc := accrual.New(repoOrder, repoAccrual, repoBalance)
+	balanceUc := balance.New(repoBalance)
+	// init server
+
+	accruarUc.StartProcessingAccruals(ctx)
+
+	httpserver := httpserver.New(cfg, authUc, ordersUc, balanceUc)
+
+	httpserver.Run(ctx)
 }
